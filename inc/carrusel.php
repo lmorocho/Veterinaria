316a838f83d653374f3f407ab184ef5edce32bf9
Luis <104954514+lmorocho@users.noreply.github.com>
2025-04-08 16:35:07 -0300

    Update_250408_1634
    Nueva Actualización

---
+++ inc/carrusel.php
@@ -0,0 +1,33 @@
+<?php
+    //Declaramos la función
+    function carrusels(){   
+?>
+
+    <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+        <img src="img/dog.png" class="d-block w-70" alt="...">
+        <!--<img src="img/dog.png" class="img-fluid" alt="...">-->
+        <!--<img src="img/russian.jpg" class="img-fluid" alt="...">-->
+        </div>
+        <div class="carousel-item">
+        <img src="img/russian.jpg" class="d-block w-70" alt="...">
+        </div>
+        <div class="carousel-item">
+        <img src="img/siberian.jpg" class="d-block w-70" alt="...">
+        </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </button>
+    </div>
+
+
+<?php
+    }
+?>
--- index.php
+++ index.php
@@ -6,7 +6,9 @@
     <title>Sistema de Veterinaria - Grupo 2 - Instituto Roselllo</title>
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <?php
-        require("inc/menu.php")
+        require("inc/menu.php");
+        require("inc/acerca.php");
+        require("inc/carrusel.php")
     ?>
 </head>
 <!--<body class="container">--> <!--Contenido dentro del container--> 
@@ -18,16 +20,31 @@
         <h4>Bienvenido al Sistema de Veterinaria</h4>
     </div>
     <div class="text-center">
-        <img src="img/dog.png" class="img-fluid" alt="...">
-    <div>
-    <div class="row">
-        <div class="col-12">
-            <!--<link href="css/style.css" rel="stylesheet">--> 
+        <!--Slides automaticos-->
+        <?php carrusels(); ?>
+        <!--<img src="img/dog.png" class="img-fluid" alt="...">-->
+        <!--<img src="img/russian.jpg" class="img-fluid" alt="...">-->
+
+        <div class="row">
+            <div class="col-12">
+                <!--<link href="css/style.css" rel="stylesheet">--> 
+            </div>
+            <div class="col-4">
+                <p></p>       
+                    <button class="btn btn-link" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling">Link1</button>
+            </div>
+            <div class="col-4">
+                <p></p>
+                <button class="btn btn-link" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling">Link2</button>
+            </div>
+            <div class="col-4">
+                <p></p>
+                <!--Barra de navegación-->
+                <?php acercade(); ?>
+            </div>
         </div>
-        <div class="col-4">Col1</div>
-        <div class="col-4">Col2</div>
-        <div class="col-4">Col3</div>
     </div>
+    
     <script src="js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
