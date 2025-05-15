c2d23fefeab0b94c6c2fa0844c837074be0cdeed
Luis <104954514+lmorocho@users.noreply.github.com>
2025-05-15 15:18:59 -0300

    20250515_Veterinaria
    Update

--- inc/dashboard_veterinaria.php
+++ inc/dashboard_veterinaria.php
@@ -3,17 +3,18 @@
     function dashboard(){   
 ?>
 
-    <div class="container">
+    <!--<div class="container">-->
         <!--<h2>Dashboard</h2>-->
         <div class="numbers">
-            <div class="text-bg-dark p-3">
+            <div class="text-bg-secondary p-3">
+                <br>
                 <!-- Titulo centrado de la seccion-->
                 <div class="row text-center">
                     <div class="col-3">
                         <div class="d-flex justify-content-center">
                         <!--<h2>Dashboard</h2>-->
                         <!--<div class="card text-bg-secondary mb-3" style="max-width: 18rem;">-->
-                            <div class="card text-bg-secondary mb-3" style="width: 18rem;">
+                            <div class="card text-bg-warning mb-3" style="width: 22rem;">
                             <!--<div background-color: #ff6445 img src="img/paws.png">-->
                                 <div class="card-header"><h6>Total Clientes</h6></div>
                                 <div class="card-body">
@@ -25,7 +26,7 @@
                     <div class="col-3">
                         <div class="d-flex justify-content-center">
                         <!--<div>Total Mascotas: <span id="total-mascotas">250</span></div>-->
-                            <div class="card text-bg-secondary mb-3" style="width: 18rem;">
+                            <div class="card text-bg-warning mb-3" style="width: 22rem;">
                                 <div class="card-header"><h6>Total Mascotas</h6></div>
                                 <div class="card-body">
                                     <p class="card-text"><h2>250</h2></p>
@@ -36,7 +37,7 @@
                     <div class="col-3">
                         <div class="d-flex justify-content-center">
                             <!--<div>Ventas del Mes: <span id="ventas-mes">$35,000</span></div>-->
-                            <div class="card text-bg-secondary mb-3" style="width: 18rem;">
+                            <div class="card text-bg-warning mb-3" style="width: 22rem;">
                                 <div class="card-header"><h6>Ventas del Mes</h6></div>
                                 <div class="card-body">
                                     <p class="card-text"><h2>$35.000 ARS</h2></p>
@@ -47,7 +48,7 @@
                     <div class="col-3">
                         <div class="d-flex justify-content-center">
                             <!--<div>Turnos Hoy: <span id="turnos-hoy">8</span></div>-->
-                            <div class="card text-bg-secondary mb-3" style="width: 18rem;">
+                            <div class="card text-bg-warning mb-3" style="width: 22rem;">
                                 <div class="card-header"><h6>Total de Turnos Hoy</h6></div>
                                 <div class="card-body">
                                     <p class="card-text"><h2>8</h2></p>
@@ -59,16 +60,16 @@
             </div>
         </div>
         <br>
-
+        
         <!--Resumenes-->
         <div class="resumenes">
-            <div class="text-bg-dark p-3">
+            <div class="text-bg-secondary p-3">
+                <br>
                 <!-- Titulo centrado de la seccion-->
-                <div class="row">
+                <div class="row text-center">
                     <div class="col-4">
                         <div class="d-flex justify-content-center">
-                        <!--<div>Total Mascotas: <span id="total-mascotas">250</span></div>-->
-                            <div class="card text-bg-secondary mb-3" style="width: 22rem;">
+                            <!--<div class="card text-bg-secondary mb-3" style="width: 28rem;">
                                 <div class="card-header text-center"><h6>Últimas Citas Médicas</h6></div>
                                 <div class="card-body">
                                     <p class="card-text"><h4>
@@ -78,13 +79,21 @@
                                         </ul>
                                     </h4></p>
                                 </div>
+                            </div>-->
+                            <div class="card text-bg-dark mb-3" style="width: 28rem;">
+                                <div class="card-header">Últimas Citas Médicas</div>
+                                <ul class="list-group list-group-flush" id="ultimas-citas">
+                                    
+                                    <li class="list-group-item">Firulais - Consulta general</li>
+                                    <li class="list-group-item">Pelusa - Vacunación</li>
+                                </ul>
                             </div>
                         </div>
                     </div>
+                    
                     <div class="col-4">
                         <div class="d-flex justify-content-center">
-                            <!--<div>Ventas del Mes: <span id="ventas-mes">$35,000</span></div>-->
-                            <div class="card text-bg-secondary mb-3" style="width: 22rem;">
+                            <!--<div class="card text-bg-secondary mb-3" style="width: 28rem;">
                                 <div class="card-header"><h6>Productos con Menor Stock</h6></div>
                                 <div class="card-body">
                                     <p class="card-text"><h4>
@@ -94,13 +103,20 @@
                                         </ul>
                                     </h4></p>
                                 </div>
+                            </div>-->
+                            <div class="card text-bg-dark mb-3" style="width: 28rem;">
+                                <div class="card-header">Productos con Menor Stock</div>
+                                <ul class="list-group list-group-flush" id="menor-stock">
+                                    <li class="list-group-item">Alimento X - 2 unidades</li>
+                                    <li class="list-group-item">Vacuna Y - 1 unidad</li>
+                                </ul>
                             </div>
-                        </div>
+                        </div>  
                     </div>
+
                     <div class="col-4">
                         <div class="d-flex justify-content-center">
-                            <!--<div>Turnos Hoy: <span id="turnos-hoy">8</span></div>-->
-                            <div class="card text-bg-secondary mb-3" style="width: 22rem;">
+                            <!--<div class="card text-bg-secondary mb-3" style="width: 28rem;">
                                 <div class="card-header"><h6>Próximos Cumpleaños de Mascotas</h6></div>
                                 <div class="card-body">
                                     <p class="card-text"><h4>
@@ -110,15 +126,21 @@
                                         </ul>
                                     </h4></p>
                                 </div>
+                            </div>-->
+                            <div class="card text-bg-dark mb-3" style="width: 28rem;">
+                                <div class="card-header">Próximos Cumpleaños de Mascotas</div>
+                                <ul class="list-group list-group-flush" id="cumple-mascotas">
+                                    <li class="list-group-item">Luna - 28 Abril</li>
+                                    <li class="list-group-item">Roco - 05 Mayo</li>
+                                </ul>
                             </div>
-                        </div>
+                        </div>   
                     </div>
                 </div>
             </div>
         </div>
         <br><br>       
-    </div>
-
+    <!--</div>-->
 
 <?php
     }
