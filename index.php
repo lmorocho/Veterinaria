a737acaf1b4fdc5e9b1e87c5664491e34c9cfe74
Luis <104954514+lmorocho@users.noreply.github.com>
2025-06-25 19:39:12 -0300

    20250625_1939_update_index
    Veterinaria
    
    Co-Authored-By: Ariel2401 <206783411+Ariel2401@users.noreply.github.com>
    Co-Authored-By: agusarufe <206754026+agusarufe@users.noreply.github.com>

--- index.php
+++ index.php
@@ -47,7 +47,7 @@
       position: fixed;
       top: 0; left: 0;
       width: 100vw; height: 100vh;
-      background: rgba(255,255,255,0.5);
+      /*background: rgba(255,255,255,0.5);*/
       pointer-events: none;
       z-index: -1;
     }
